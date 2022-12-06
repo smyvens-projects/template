d3621106ab42b01ac31cbe9257d44e454eabe1ac
styled <smyvensestime@gmail.com>
2022-12-06 18:02:45 -0500

    formatting

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+/**
+ * @type {import('eslint').ESLint}
+ */
+
+module.exports = {
+    root: true,
+    extends: [
+        "next/core-web-vitals",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "airbnb",
+        "airbnb-typescript",
+        "plugin:import/recommended",
+        "plugin:prettier/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./tsconfig.json",
+        ecmaVersion: 2020,
+        ecmaFeatures: {
+            jsx: false,
+        },
+    },
+    plugins: ["import", "@typescript-eslint"],
+    rules: {
+        "no-console": "error",
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-props-no-spreading": "off",
+        "prettier/prettier": "off",
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+    env: {
+        browser: true,
+        node: true,
+    },
+    ignorePatterns: ["**/*.js", "**/*.json", "node_modules", ".next", "public"],
+}
