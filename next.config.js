b1bf412e164fe53a71e1aac8f224390a858f8a1e
styled <smyvensestime@gmail.com>
2022-12-07 12:09:02 -0500

    fix wrong basepath

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = (phase, { defaultConfig }) => {
         experimental: {
             appDir: true,
         },
-        basePath: "/account",
+        basePath: "/",
     }
 
     return config
