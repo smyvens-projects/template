9d8a032e21246b01081a7f488db1391ea1d35baf
styled <smyvensestime@gmail.com>
2022-12-10 12:48:27 -0500

    better default page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default async function Page() {
     return (
         <div className="w-full h-screen flex items-center justify-center flex-col gap-3">
-            <h1 className="bold text-6xl text-primary font-gilroy">Hello World</h1>
-            <h3 className="bold text-6xl text-primary font-poppins">So happy to have you</h3>
+            <h1 className="bold text-6xl text-primary font-gilroy font-bold">Hello World</h1>
+            <h3 className="bold text-3xl text-primary font-poppins">So happy to have you</h3>
         </div>
     )
 }
